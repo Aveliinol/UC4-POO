20167893ad00adfd354aff76c59ec38918942b2c
xexeu.l <lugabriell084@outlook.com.br>
2024-11-14 17:04:47 -0300

    EDITS: editei bastante coisa

--- aula13-11/src/controller/controllerNotebook.js
+++ aula13-11/src/controller/controllerNotebook.js
@@ -5,62 +5,85 @@ const {Notebook} = require('../models/Notebook');
 class ControllerNotebook{
     criarNotebook(numeroDeSerie, marca, modelo, cor, valor){
         try {
-            const novoNotebook = new Notebook(numeroDeSerie, marca,
-                 modelo, cor, valor); 
-                 dataBaseNotebook.push(novoNotebook);
-                 console.log(`Notebook ${this.getNumeroDeSerie} foi adicionado com sucesso!`)
-        } catch (error) { 
-           console.error('Erro ao criar um novo Notebook', error.message)
+            const novoNotebook = new Notebook(numeroDeSerie, marca, modelo, cor, valor);
+            dataBaseNotebook.push(novoNotebook);
+            console.log("Notebook adicionado com sucesso!");
+            return novoNotebook
+        } catch (error) {
+            console.error("Erro ao adicionar notebook", error.message);
         }
     }
 
-    listarNotebook(){
+    listarNotebooks(){
         try {
-            const listaNotebook = dataBaseNotebook.map( Notebook => {
-                numeroDeSerie: Notebook.getNumeroDeSerie;
-                marca: Notebook.marca;
-                modelo: Notebook.modelo;
-                cor: Notebook.cor;
-                valor: Notebook.getValor;
-
-            });
-            console.table(listaNotebook)
+            const notebooks = dataBaseNotebook.map( notebook => ({
+                numeroDeSerie: notebook.getNumeroDeSerie,
+                marca: notebook.marca,
+                modelo: notebook.modelo,
+                cor: notebook.cor,
+                valor: notebook.getValor
+            }))
+            console.table(notebooks);
         } catch (error) {
-            console.error('Erro ao lista Notebooks', error.message);
+            console.error("Erro ao listar notebooks", error.message);
+        }
+    }
+
+    buscarNotebook(numeroDeSerie){
+        try{
+            const notebook =  dataBaseNotebook.find(n => n.getNumeroDeSerie === numeroDeSerie);
+            if(!notebook){
+                console.log("Notebook não encontrado.", numeroDeSerie);
+                return;
+            }
+            console.log('Notebook encontrado:');
+            console.table([notebook]);
+            return notebook;
+        }catch(error){
+            console.error("Erro ao buscar notebook:", error.message);
         }
     }
 
     editarNotebook(numeroDeSerie, marca, modelo, cor, valor){
         try {
-            const notebook = Notebook.find(notebook => Notebook.getNumeroDeSerie 
-                === numeroDeSerie);
-                if(notebook){
-                    Notebook.marca = novaMarca || Notebook.marca
-                    Notebook.modelo = novoModelo || Notebook.modelo
-                    Notebook.cor = novaCor || Notebook.cor
-                    Notebook.getValor = novoValor || Notebook.getValor
-                }else {
-                    console.log('Notebook não encontrado!')
-                }
+            const notebook = this.buscarNotebook(numeroDeSerie);
+               if(notebook){
+                notebook.marca = novaMarca;
+                notebook.modelo = novoModelo;
+                notebook.cor = novaCor;
+                notebook.setValor = novoValor;
+                return notebook;
+               }
 
         } catch (error) {
             console.error('Erro ao tenta editar notebook', error.message);
         }
     }
 
-    excluirNotebook(){
+    excluirNotebook(numeroDeSerie){
         try {
-            
+            const notebook = this.buscarNotebook(numeroDeSerie);
+              if(notebook){
+                const index = dataBaseNotebook.findIndex(n => n.getNumeroDeSerie === numeroDeSerie);
+                const notebookRemovido = dataBaseNotebook.splice(index, 1);
+                console.log('Excluido com sucesso')
+                return notebookRemovido;     
+
+              }
         } catch (error) {
-            
+            console.error('Erro ao remover o notebook do sistema', error.message)
         }
     }
 
     excluirTodosNotebook(){
         try {
-            
+            if(dataBaseNotebook.length > 0){
+                dataBaseNotebook.length = 0;
+            };
         } catch (error) {
-            
+            console.error('Erro ao tenta excluir tudo', error.message);
         }
     }
 }
+
+module.exports = {ControllerNotebook};
--- aula13-11/src/models/Estoque.js
+++ aula13-11/src/models/Estoque.js
@@ -2,10 +2,28 @@ const {Notebook} = require('./Notebook')
 
 class Estoque{
 
-    constructor(produtos, quantidade){
-        this.produtos = produtos;
-        this.quantidade = quantidade;
+    #id;
+
+    constructor(id, produtos, quantidade){3
+        try {
+            if(produtos instanceof Notebook)
+                this.#id = id;
+            this.produtos = produtos;
+            this.quantidade = quantidade;
+        } catch (error) {
+            console.error('Não é permitido criar Estoque');
+        }
+       
     }
+
+    get getId(){
+        return this.#id
+    }
+
+    set setId(novoId){
+        this.#id = novoId
+    }
+
 }
 
 module.exports = {Estoque}
